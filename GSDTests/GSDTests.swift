59df51de0a8b766461f984ee22c1564f65559a62
Asaf Navon <asnav1999@gmail.com>
2025-05-15 11:30:14 +0300

    Initial Commit

---
+++ GSDTests/GSDTests.swift
@@ -0,0 +1,17 @@
+//
+//  GSDTests.swift
+//  GSDTests
+//
+//  Created by Asaf Navon on 5/15/25.
+//
+
+import Testing
+@testable import GSD
+
+struct GSDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
